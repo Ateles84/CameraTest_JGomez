70a3ea91410b2ee0dafa5f2104dd7f25fd4c2ab6
Joan <juanbass84@gmail.com>
2019-12-18 14:21:54 +0100

    ara si, tot funcionant

--- app/src/main/java/com/example/camaratest/MainActivity.java
+++ app/src/main/java/com/example/camaratest/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.camaratest;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.media.Image;
 import android.net.Uri;
@@ -17,6 +24,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -37,10 +45,21 @@ public class MainActivity extends AppCompatActivity {
         iv = findViewById(R.id.foto);
         btn = findViewById((R.id.btnFoto));
 
+        btn.setOnClickListener(new View.OnClickListener() { //Ens assegurem que tenim els permisos d'escriptura, ja que sino s'han de permetre manualment
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    dispatchTakePictureIntent(v);
+                } else {
+                    demanarPermissosEmm();
+                }
+            }
+        });
+
 
     }
 
-    public void dispatchTakePictureIntent(View v) {
+    public void dispatchTakePictureIntent(View v) {     //Codi de la documentació de google, una mica adaptat
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -50,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 photoFile = createImageFile();
             } catch (IOException ex) {
                 // Error occurred while creating the File
-                Log.v("error",("Error: "+ex.getMessage()));
+                Log.v("error", ("Error: " + ex.getMessage()));
                 ex.printStackTrace();
             }
             // Continue only if the File was successfully created
@@ -82,8 +101,11 @@ public class MainActivity extends AppCompatActivity {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = new File(getFilesDir(),"fotos");
-        Log.v("dirUtilitzat: ",storageDir.getPath());
+
+
+        File storageDir = new File(getFilesDir(), "fotos");
+
+        Log.v("dirUtilitzat: ", String.valueOf(storageDir.mkdir()));
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -94,5 +116,42 @@ public class MainActivity extends AppCompatActivity {
         currentPhotoPath = image.getAbsolutePath();
         Log.v("image", image.getPath());
         return image;
+    }
+
+
+    //DEMANAR PERMISSOS
+
+
+    private void demanarPermissosEmm() {    //Aquest codi basicament crea un dialog per a demanar els permisos en cas de que no estiguin acceptats, no deixa fer la foto sense els permissos
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(this).setTitle("Ep!")
+                    .setMessage("Es necessiten permisos per escriure al dispositiu")
+                    .setPositiveButton("endevant", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                        }
+                    }).setNegativeButton("Enrere", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).create().show();
+
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                Toast.makeText(this, "Accepta els permisos", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
